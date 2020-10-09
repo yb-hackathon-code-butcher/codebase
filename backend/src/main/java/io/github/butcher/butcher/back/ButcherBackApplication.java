413b155f3c71d167cad5742537b9d8182826db26
Timon Borter <timon.borter@gmx.ch>
2020-10-09 13:40:09 +0200

    initial backend from spring starter.

---
+++ backend/src/main/java/io/github/butcher/butcher/back/ButcherBackApplication.java
@@ -0,0 +1,12 @@
+package io.github.butcher.butcher.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButcherBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ButcherBackApplication.class, args);
+  }
+}
---
+++ backend/src/test/java/io/github/butcher/butcher/back/ButcherBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.butcher.butcher.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButcherBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
