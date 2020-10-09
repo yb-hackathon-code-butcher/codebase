e3e943f919611ba5dd8f96feaa458fd198554215
doxsch <28098153+doxsch@users.noreply.github.com>
2020-10-09 20:53:20 +0200

    added socket models added message types added ngx-socket-io

--- frontend/butcher-front/src/environments/environment.prod.ts
+++ frontend/butcher-front/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  socketEndpoint: 'http://socketurl',
+  socketIoOptions: {
+    "reconnection": true,             // whether to reconnect automatically
+    "reconnectionAttempts": 1000, // number of reconnection attempts before giving up
+    "reconnectionDelay": 1000,        // how long to initially wait before attempting a new reconnection
+    "reconnectionDelayMax": 5000,     // maximum amount of time to wait between reconnection attempts. Each attempt increases the reconnection delay by 2x along with a randomization factor
+    "randomizationFactor": 0.5,
+  }
 };
