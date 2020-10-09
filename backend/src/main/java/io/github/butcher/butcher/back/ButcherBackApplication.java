f94b97e4846ba378d2a5ce4674dacfdae806812b
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2020-10-10 00:18:02 +0200

    added springbootservletinitializer

--- backend/src/main/java/io/github/butcher/butcher/back/ButcherBackApplication.java
+++ backend/src/main/java/io/github/butcher/butcher/back/ButcherBackApplication.java
@@ -2,10 +2,16 @@ package io.github.butcher.butcher.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ButcherBackApplication {
-
+public class ButcherBackApplication extends SpringBootServletInitializer {
+  @Override
+  protected SpringApplicationBuilder configure(
+          SpringApplicationBuilder builder) {
+    return builder.sources(ButcherBackApplication.class);
+  }
   public static void main(String[] args) {
     SpringApplication.run(ButcherBackApplication.class, args);
   }
