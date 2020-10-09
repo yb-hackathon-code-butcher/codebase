8ce3fd16bbbf1de13f5cea86c1c2d1d0f665e072
Timon Borter <timon.borter@gmx.ch>
2020-10-09 16:16:23 +0200

    remodel server to netty and webflux.

---
+++ backend/src/test/java/io/github/butcher/butcher/back/ButcherBackApplicationIntegrationTest.java
@@ -0,0 +1,15 @@
+package io.github.butcher.butcher.back;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ButcherBackApplicationIntegrationTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
