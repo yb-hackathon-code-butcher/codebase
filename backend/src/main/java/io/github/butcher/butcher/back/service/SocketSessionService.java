9023d57b87f53a9f91b9e6012ec7def3e019c2a5
Timon Borter <timon.borter@gmx.ch>
2020-10-09 20:14:29 +0200

    broadcast events per room.

--- backend/src/main/java/io/github/butcher/butcher/back/service/SocketSessionService.java
+++ backend/src/main/java/io/github/butcher/butcher/back/service/SocketSessionService.java
@@ -1,24 +1,27 @@
 package io.github.butcher.butcher.back.service;
 
 import com.corundumstudio.socketio.SocketIOClient;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SocketSessionService {
 
-  private static final Map<Long, List<SocketIOClient>> SOCKET_IO_CONNECTIONS_PER_TEAM = new HashMap<>();
+  private static final Logger LOGGER = LoggerFactory.getLogger(SocketSessionService.class);
+
+  public static final String TEAM_ROOM_PREFIX = "team-";
 
   public void registerConnection(SocketIOClient socketIOClient) {
-    SOCKET_IO_CONNECTIONS_PER_TEAM.get(getTeamIdForClient(socketIOClient))
-        .add(socketIOClient);
+    Long teamId = getTeamIdForClient(socketIOClient);
+    LOGGER.debug("Client connected.. joins team {}", teamId);
+    socketIOClient.joinRoom(TEAM_ROOM_PREFIX + teamId);
   }
 
   public void clientDisconnected(SocketIOClient socketIOClient) {
-    SOCKET_IO_CONNECTIONS_PER_TEAM.get(getTeamIdForClient(socketIOClient))
-        .remove(socketIOClient);
+    Long teamId = getTeamIdForClient(socketIOClient);
+    LOGGER.debug("Client disconnected.. leaves team {}", teamId);
+    socketIOClient.joinRoom(TEAM_ROOM_PREFIX + teamId);
   }
 
   private Long getTeamIdForClient(SocketIOClient socketIOClient) {
--- backend/src/main/java/io/github/butcher/butcher/back/socket/SocketIOServerOutboundEventListener.java
+++ backend/src/main/java/io/github/butcher/butcher/back/socket/SocketIOServerOutboundEventListener.java
@@ -1,6 +1,8 @@
 package io.github.butcher.butcher.back.socket;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import io.github.butcher.butcher.back.service.GameStateService;
+import io.github.butcher.butcher.back.service.SocketSessionService;
 import io.github.butcher.butcher.back.socket.event.SocketIOEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -9,15 +11,19 @@ import org.springframework.stereotype.Component;
 public class SocketIOServerOutboundEventListener {
 
   private final SocketIOServer socketIOServer;
+  private final GameStateService gameStateService;
 
-  public SocketIOServerOutboundEventListener(SocketIOServer socketIOServer) {
+  public SocketIOServerOutboundEventListener(SocketIOServer socketIOServer,
+      GameStateService gameStateService) {
     this.socketIOServer = socketIOServer;
+    this.gameStateService = gameStateService;
   }
 
   @EventListener
   public void broadcastEvent(SocketIOEvent socketIOEvent) {
-    // TODO: This must be team specific?
-    socketIOServer.getBroadcastOperations()
-        .sendEvent(socketIOEvent.getStringId(), socketIOEvent);
+    gameStateService.getActiveTeamIds().forEach(activeTeamId -> {
+      socketIOServer.getRoomOperations(SocketSessionService.TEAM_ROOM_PREFIX + activeTeamId)
+          .sendEvent(socketIOEvent.getStringId(), socketIOEvent);
+    });
   }
 }
