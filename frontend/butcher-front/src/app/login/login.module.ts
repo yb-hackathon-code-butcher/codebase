638bf8f4e23b3e2d9107adffafc5fd78ae80bff9
Pascal Linder <pascal.linder97@hotmail.com>
2020-10-09 17:22:33 +0200

    login component + service

--- frontend/butcher-front/src/app/login/login.module.ts
+++ frontend/butcher-front/src/app/login/login.module.ts
@@ -3,13 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
+  ],
+  providers:[
   ]
 })
-export class LoginModule { }
+export class LoginModule {
+}
