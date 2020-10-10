f2af9ab10a3e431255cea7bbd1902d14a8468545
Timon Borter <timon.borter@gmx.ch>
2020-10-10 05:45:50 +0200

    invalid foreign key.

--- backend/src/main/resources/db/migration/V8__game_saves_current_possession.sql
+++ backend/src/main/resources/db/migration/V8__game_saves_current_possession.sql
@@ -1,3 +1,3 @@
 ALTER TABLE game
     ADD COLUMN possession_team_id BIGINT,
-    ADD CONSTRAINT fk_team1 FOREIGN KEY (possession_team_id) REFERENCES team (id);
+    ADD CONSTRAINT fk_team3 FOREIGN KEY (possession_team_id) REFERENCES team (id);
