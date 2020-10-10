5943f728e0c0a808890b1d48fd4078684b1119b4
doxsch <28098153+doxsch@users.noreply.github.com>
2020-10-10 02:09:51 +0200

    added more components

---
+++ frontend/butcher-front/src/app/game/option-list/option-list.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-option-list',
+  templateUrl: './option-list.component.html',
+  styleUrls: ['./option-list.component.scss']
+})
+export class OptionListComponent implements OnInit {
+  optionLetters = ['A', 'B', 'C', 'D'];
+  @Input()
+  teamName: string;
+  @Input()
+  options: {name: string, chance: number}[];
+  @Input()
+  tempResults
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
