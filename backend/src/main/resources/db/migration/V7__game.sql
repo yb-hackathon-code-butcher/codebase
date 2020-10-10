264d317ee6a4dbb54741c7a6c9396583c3bc3f21
Jan Heimberg <jan.heimberg@gmx.ch>
2020-10-10 04:09:18 +0200

    fix

--- backend/src/main/resources/db/migration/V7__game.sql
+++ backend/src/main/resources/db/migration/V7__game.sql
@@ -1,4 +1,4 @@
-INSERT INTO game(team_id, name)
+INSERT INTO game(start_time, end_time, team1_id, team2_id, team1_score, team2_score, round_end_time)
 VALUES
-    (1, current_timestamp + (interval '1' minute), current_timestamp + (interval '10' minute),
+    (current_timestamp + (interval '1' minute), current_timestamp + (interval '10' minute),
     1,2,3,0, current_timestamp + (interval '80' second))
