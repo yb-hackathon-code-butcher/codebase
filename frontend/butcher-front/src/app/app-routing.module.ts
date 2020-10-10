92f885fed05d4068c91c7498ece9e51735e2ba90
doxsch <28098153+doxsch@users.noreply.github.com>
2020-10-10 06:51:34 +0200

    several improvements

--- frontend/butcher-front/src/app/app-routing.module.ts
+++ frontend/butcher-front/src/app/app-routing.module.ts
@@ -1,30 +1,37 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [{
-  path: 'login',
-  loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
-}, {
-  path: 'countdown',
-  loadChildren: () => import('./countdown/countdown.module').then(m => m.CountdownModule)
-}, {
-  path: 'gameEnd',
-  loadChildren: () => import('./game-end/game-end.module').then(m => m.GameEndModule)
-},
+const routes: Routes = [
+
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  }, {
+    path: 'countdown',
+    loadChildren: () => import('./countdown/countdown.module').then(m => m.CountdownModule)
+  }, {
+    path: 'game-end',
+    loadChildren: () => import('./game-end/game-end.module').then(m => m.GameEndModule)
+  },
   {
     path: 'game',
     loadChildren: () => import('./game/game.module').then(m => m.GameModule)
-  },{
-  path: 'team-selection',
+  }, {
+    path: 'team-selection',
     loadChildren: () => import('./team-selection/team-selection.module').then(m => m.TeamSelectionModule)
   }, {
-  path: 'countdown',
+    path: 'countdown',
     loadChildren: () => import('./countdown/countdown.module').then(m => m.CountdownModule)
-  },{
-  path: 'coinflip',
-  loadChildren: () => import('./coinflip/coinflip.module').then(m => m.CoinflipModule)
-},
-  { path: 'round-end', loadChildren: () => import('./round-end/round-end.module').then(m => m.RoundEndModule) }];
+  }, {
+    path: 'coinflip',
+    loadChildren: () => import('./coinflip/coinflip.module').then(m => m.CoinflipModule)
+  },
+  {path: 'round-end', loadChildren: () => import('./round-end/round-end.module').then(m => m.RoundEndModule)},
+
+  {
+    path: '**',
+    redirectTo: 'login'
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
