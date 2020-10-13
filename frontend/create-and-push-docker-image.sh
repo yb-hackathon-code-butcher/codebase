cd084a952d9ee2b598b0e978947ab5c58c26937f
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2020-10-13 23:28:11 +0200

    changed gke project name and temporarily changed actuator port from 8090 to 8080 so the deployment still runs until a way is found for the ingress to check on port 8090

--- frontend/create-and-push-docker-image.sh
+++ frontend/create-and-push-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # just for testing purposes, will add a github action later
-docker build -t gcr.io/jovial-syntax-292308/haubzit-frontend:latest .
-docker push gcr.io/jovial-syntax-292308/haubzit-frontend:latest
+docker build -t gcr.io/haubzit/haubzit-frontend:latest .
+docker push gcr.io/haubzit/haubzit-frontend:latest
